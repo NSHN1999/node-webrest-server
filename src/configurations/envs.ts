a4cd8ef661a62f19409baac5d3822ac28054e2a5
NicolasHernandez <constnicodeveloper@gmail.com>
2026-06-12 22:57:56 -0400

    bugfix(envs):changhe-port-value-to-default-3000

--- src/configurations/envs.ts
+++ src/configurations/envs.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import env from "env-var";
 
 export const envs = {
-  PORT: env.get('PORT').required().asPortNumber(),
+  PORT: env.get('PORT').default(3000).asPortNumber(),
   PUBLIC_PATH: env.get('PUBLIC_PATH').default('public').asString(),
   POSTGRES_URL: env.get('POSTGRES_URL').required().asUrlString(),
 };
